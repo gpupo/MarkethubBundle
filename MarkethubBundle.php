35bf3d8cf8472ed140a84a30a1cae5d8184684c6
Gilmar Pupo <g@g1mr.com>
2015-04-08 09:43:21 -0300

    DI Configuration

---
+++ MarkethubBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of gpupo/markethub-bundle
+ *
+ * (c) Gilmar Pupo <g@g1mr.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @version 1.0
+ */
+
+namespace Gpupo\Bundle\MarkethubBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MarkethubBundle extends Bundle
+{
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# MarketplaceHubBundle
+# MarkethubBundle
+
+
+## Setup 
+
+app/config/parameters.yml :
+
+```yaml
+parameters:
+    markethub_submarino_api_token: 'foo'
+```
+
+app/config/config.yml:
+
+```yaml
+markethub:
+    enabled: true
+    submarino:
+        api_token: %markethub_submarino_api_token%
+        api_version: 'sandbox'        
+```
