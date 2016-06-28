48b51fe4023b5d71917e2293a3065d4fc653ead6
Gilmar Pupo <contact@gpupo.com>
2016-06-28 09:46:56 -0300

    doc files

--- MarkethubBundle.php
+++ MarkethubBundle.php
@@ -2,14 +2,12 @@
 
 /*
  * This file is part of gpupo/markethub-bundle
- *
- * (c) Gilmar Pupo <g@g1mr.com>
- *
+ * Created by Gilmar Pupo <g@g1mr.com>
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
- * @version 1.0
+ * For more information, see <http://www.g1mr.com/>.
  */
+
 namespace Gpupo\Bundle\MarkethubBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
