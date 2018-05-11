fe70d2cdee0e488320fa0355426d9aa43dc57352
Gilmar Pupo <contact@gpupo.com>
2018-05-11 10:14:59 -0300

    Apply CS rules

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,15 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of gpupo/markethub-bundle
- * Created by Gilmar Pupo <g@g1mr.com>
+ * Created by Gilmar Pupo <contact@gpupo.com>
  * For the information of copyright and license you should read the file
  * LICENSE which is distributed with this source code.
  * Para a informação dos direitos autorais e de licença você deve ler o arquivo
  * LICENSE que é distribuído com este código-fonte.
  * Para obtener la información de los derechos de autor y la licencia debe leer
  * el archivo LICENSE que se distribuye con el código fuente.
- * For more information, see <http://www.g1mr.com/>.
+ * For more information, see <https://opensource.gpupo.com/>.
+ *
  */
 
 namespace Gpupo\Bundle\MarkethubBundle\DependencyInjection;
@@ -25,72 +28,72 @@ class Configuration implements ConfigurationInterface
         $treeBuilder
             ->root('markethub')
             ->children()
-                ->booleanNode('enabled')->defaultTrue()->end()
+            ->booleanNode('enabled')->defaultTrue()->end()
                 //submarino-sdk eof
-                ->arrayNode('submarino')
-                    ->children()
-                        ->booleanNode('enabled')->defaultFalse()->end()
-                        ->scalarNode('api_token')->defaultValue('')->end()
-                        ->scalarNode('api_version')->defaultValue('sandbox')->end()
-                        ->booleanNode('verbose')->defaultFalse()->end()
-                        ->scalarNode('ssl_version')->defaultValue('SecureTransport')->end()
-                    ->end()
-                ->end()
+            ->arrayNode('submarino')
+            ->children()
+            ->booleanNode('enabled')->defaultFalse()->end()
+            ->scalarNode('api_token')->defaultValue('')->end()
+            ->scalarNode('api_version')->defaultValue('sandbox')->end()
+            ->booleanNode('verbose')->defaultFalse()->end()
+            ->scalarNode('ssl_version')->defaultValue('SecureTransport')->end()
+            ->end()
+            ->end()
                 //submarino-sdk eof
                 //cnova-sdk
-                ->arrayNode('cnova')
-                    ->children()
-                        ->booleanNode('enabled')->defaultFalse()->end()
-                        ->scalarNode('client_id')->defaultValue('')->end()
-                        ->scalarNode('access_token')->defaultValue('')->end()
-                        ->scalarNode('api_version')->defaultValue('sandbox')->end()
-                        ->booleanNode('verbose')->defaultFalse()->end()
-                        ->scalarNode('register_path')->defaultValue('')->end()
-                        ->scalarNode('ssl_version')->defaultValue('SecureTransport')->end()
-                        ->booleanNode('ssl_verify_peer')->defaultTrue()->end()
-                    ->end()
-                ->end()
+            ->arrayNode('cnova')
+            ->children()
+            ->booleanNode('enabled')->defaultFalse()->end()
+            ->scalarNode('client_id')->defaultValue('')->end()
+            ->scalarNode('access_token')->defaultValue('')->end()
+            ->scalarNode('api_version')->defaultValue('sandbox')->end()
+            ->booleanNode('verbose')->defaultFalse()->end()
+            ->scalarNode('register_path')->defaultValue('')->end()
+            ->scalarNode('ssl_version')->defaultValue('SecureTransport')->end()
+            ->booleanNode('ssl_verify_peer')->defaultTrue()->end()
+            ->end()
+            ->end()
                 //cnova-sdk eof
                 //netshoes-sdk
-                ->arrayNode('netshoes')
-                    ->children()
-                        ->booleanNode('enabled')->defaultFalse()->end()
-                        ->scalarNode('client_id')->defaultValue('')->end()
-                        ->scalarNode('access_token')->defaultValue('')->end()
-                        ->scalarNode('api_version')->defaultValue('sandbox')->end()
-                        ->booleanNode('verbose')->defaultFalse()->end()
-                        ->scalarNode('register_path')->defaultValue('')->end()
-                        ->scalarNode('ssl_version')->defaultValue('SecureTransport')->end()
-                        ->booleanNode('ssl_verify_peer')->defaultTrue()->end()
-                    ->end()
-                ->end()
+            ->arrayNode('netshoes')
+            ->children()
+            ->booleanNode('enabled')->defaultFalse()->end()
+            ->scalarNode('client_id')->defaultValue('')->end()
+            ->scalarNode('access_token')->defaultValue('')->end()
+            ->scalarNode('api_version')->defaultValue('sandbox')->end()
+            ->booleanNode('verbose')->defaultFalse()->end()
+            ->scalarNode('register_path')->defaultValue('')->end()
+            ->scalarNode('ssl_version')->defaultValue('SecureTransport')->end()
+            ->booleanNode('ssl_verify_peer')->defaultTrue()->end()
+            ->end()
+            ->end()
                 //netshoes-sdk eof
                 //mercadolivre-sdk
-                ->arrayNode('mercadolivre')
-                    ->children()
-                        ->booleanNode('enabled')->defaultFalse()->end()
-                        ->scalarNode('client_id')->defaultValue('')->end()
-                        ->scalarNode('access_token')->defaultValue('')->end()
-                        ->scalarNode('api_version')->defaultValue('sandbox')->end()
-                        ->booleanNode('verbose')->defaultFalse()->end()
-                        ->scalarNode('register_path')->defaultValue('')->end()
-                        ->scalarNode('ssl_version')->defaultValue('SecureTransport')->end()
-                        ->booleanNode('ssl_verify_peer')->defaultTrue()->end()
-                    ->end()
-                ->end()
+            ->arrayNode('mercadolivre')
+            ->children()
+            ->booleanNode('enabled')->defaultFalse()->end()
+            ->scalarNode('client_id')->defaultValue('')->end()
+            ->scalarNode('access_token')->defaultValue('')->end()
+            ->scalarNode('api_version')->defaultValue('sandbox')->end()
+            ->booleanNode('verbose')->defaultFalse()->end()
+            ->scalarNode('register_path')->defaultValue('')->end()
+            ->scalarNode('ssl_version')->defaultValue('SecureTransport')->end()
+            ->booleanNode('ssl_verify_peer')->defaultTrue()->end()
+            ->end()
+            ->end()
                 //mercadolivre-sdk eof
                 //brandlovers-sdk
-                ->arrayNode('brandlovers')
-                    ->children()
-                        ->booleanNode('enabled')->defaultFalse()->end()
-                        ->scalarNode('authorization')->defaultValue('')->end()
-                        ->scalarNode('api_version')->defaultValue('sandbox')->end()
-                        ->booleanNode('verbose')->defaultFalse()->end()
-                        ->scalarNode('register_path')->defaultValue('')->end()
-                        ->scalarNode('ssl_version')->defaultValue('SecureTransport')->end()
-                        ->booleanNode('ssl_verify_peer')->defaultTrue()->end()
-                    ->end()
-                ->end()
+            ->arrayNode('brandlovers')
+            ->children()
+            ->booleanNode('enabled')->defaultFalse()->end()
+            ->scalarNode('authorization')->defaultValue('')->end()
+            ->scalarNode('api_version')->defaultValue('sandbox')->end()
+            ->booleanNode('verbose')->defaultFalse()->end()
+            ->scalarNode('register_path')->defaultValue('')->end()
+            ->scalarNode('ssl_version')->defaultValue('SecureTransport')->end()
+            ->booleanNode('ssl_verify_peer')->defaultTrue()->end()
+            ->end()
+            ->end()
                 //brandlovers-sdk eof
             ->end();
 
--- Factory/CnovaFactory.php
+++ Factory/CnovaFactory.php
@@ -1,15 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of gpupo/markethub-bundle
- * Created by Gilmar Pupo <g@g1mr.com>
+ * Created by Gilmar Pupo <contact@gpupo.com>
  * For the information of copyright and license you should read the file
  * LICENSE which is distributed with this source code.
  * Para a informação dos direitos autorais e de licença você deve ler o arquivo
  * LICENSE que é distribuído com este código-fonte.
  * Para obtener la información de los derechos de autor y la licencia debe leer
  * el archivo LICENSE que se distribuye con el código fuente.
- * For more information, see <http://www.g1mr.com/>.
+ * For more information, see <https://opensource.gpupo.com/>.
+ *
  */
 
 namespace Gpupo\Bundle\MarkethubBundle\Factory;
@@ -21,13 +24,13 @@ class CnovaFactory extends Factory
     public function setClient(array $clientOptions = null)
     {
         $clientOptions = [
-            'client_id'     => $this->options['client_id'],
-            'access_token'  => $this->options['access_token'],
-            'version'       => $this->options['api_version'],
-            'verbose'       => $this->options['verbose'],
-            'sslVersion'    => $this->options['ssl_version'],
+            'client_id' => $this->options['client_id'],
+            'access_token' => $this->options['access_token'],
+            'version' => $this->options['api_version'],
+            'verbose' => $this->options['verbose'],
+            'sslVersion' => $this->options['ssl_version'],
             'sslVerifyPeer' => $this->options['ssl_verify_peer'],
-            'registerPath'  => $this->options['register_path'],
+            'registerPath' => $this->options['register_path'],
         ];
 
         return parent::setClient($clientOptions);
