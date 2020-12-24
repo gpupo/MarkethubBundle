d2ba46cae1f6196c2104b3217e9c796ca513f356
Gilmar Pupo <contact@gpupo.com>
2020-12-24 09:32:54 -0300

    Apply CS config

--- src/Bridge/Symfony/Bundle/DependencyInjection/MarkethubExtension.php
+++ src/Bridge/Symfony/Bundle/DependencyInjection/MarkethubExtension.php
@@ -3,16 +3,9 @@
 declare(strict_types=1);
 
 /*
- * This file is part of gpupo/MarketBundle
- * Created by Gilmar Pupo <contact@gpupo.com>
- * For the information of copyright and license you should read the file
- * LICENSE which is distributed with this source code.
- * Para a informação dos direitos autorais e de licença você deve ler o arquivo
- * LICENSE que é distribuído com este código-fonte.
- * Para obtener la información de los derechos de autor y la licencia debe leer
- * el archivo LICENSE que se distribuye con el código fuente.
- * For more information, see <https://opensource.gpupo.com/>.
- *
+ * This file is part of gpupo/MarketBundle created by Gilmar Pupo <contact@gpupo.com>
+ * For the information of copyright and license you should read the file LICENSE which is
+ * distributed with this source code. For more information, see <https://opensource.gpupo.com/>
  */
 
 namespace Gpupo\MarkethubBundle\Bridge\Symfony\Bundle\DependencyInjection;
@@ -46,7 +39,7 @@ class MarkethubExtension extends Extension
         $configuration = $this->getConfiguration($configs, $container);
         $config = $this->processConfiguration($configuration, $configs);
         foreach ($sdks as $sdk) {
-            if (array_key_exists($sdk, $config)) {
+            if (\array_key_exists($sdk, $config)) {
                 $parameters = $config[$sdk];
             } else {
                 $parameters = $container->getParameter(sprintf('markethub.%s', $sdk));
--- tests/Factory/SubmarinoFactoryTest.php
+++ tests/Factory/SubmarinoFactoryTest.php
@@ -3,22 +3,15 @@
 declare(strict_types=1);
 
 /*
- * This file is part of gpupo/MarketBundle
- * Created by Gilmar Pupo <contact@gpupo.com>
- * For the information of copyright and license you should read the file
- * LICENSE which is distributed with this source code.
- * Para a informação dos direitos autorais e de licença você deve ler o arquivo
- * LICENSE que é distribuído com este código-fonte.
- * Para obtener la información de los derechos de autor y la licencia debe leer
- * el archivo LICENSE que se distribuye con el código fuente.
- * For more information, see <https://opensource.gpupo.com/>.
- *
+ * This file is part of gpupo/MarketBundle created by Gilmar Pupo <contact@gpupo.com>
+ * For the information of copyright and license you should read the file LICENSE which is
+ * distributed with this source code. For more information, see <https://opensource.gpupo.com/>
  */
 
 namespace Gpupo\MarkethubBundle\Tests\Factory;
 
-use Gpupo\CommonSchema\ORM\Entity\Trading\Order\Order;
 use Gpupo\CommonSchema\ORM\Entity\Catalog\Product\Product;
+use Gpupo\CommonSchema\ORM\Entity\Trading\Order\Order;
 use Gpupo\MarkethubBundle\Factory\SubmarinoFactory;
 
 /**
